50ff9b0c86016d936b4b5ee008e8e4a9ec5196dd
Aidan <astaff21@charlotte.edu>
2025-04-23 13:42:46 -0400

    3:43:00 commit

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -73,14 +73,16 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
 
     if request.method == 'POST':
@@ -140,7 +142,9 @@ def deleteRoom(request, pk):
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
 
-
+def userProfile(request):
+    context = {}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url='/login')
 def deleteMessage(request, pk):
